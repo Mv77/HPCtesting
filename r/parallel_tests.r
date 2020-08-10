7b347046d5f718018df0146f896736673cc9f732
MateoVG <mateovegi@gmail.com>
2020-08-10 17:41:08 -0400

    Seems like mat is singular-ish

--- r/parallel_tests.r
+++ r/parallel_tests.r
@@ -4,15 +4,21 @@ library(parallel)
 test_func <- function(size){
   
   mat <- matrix(1:(size^2), nrow = size)/size^2
-  mat <- solve(mat %*% t(mat))
+  mat <- mat %*% t(mat)
+  mat <- solve(mat)
   return(sum(mat))
   
 }
 
-test_func(4)
-
 n <- 50
-size <- 10
+size <- 2
+
+# Sequential
+############
+start <- Sys.time()
+res <- lapply(X = 1:n, FUN = function(x) test_func(size))
+print(paste('Sequential time was',
+            Sys.time()-start))
 
 # Parallel
 ##########
@@ -23,16 +29,11 @@ print(paste('There are',cores,'threads available'))
 start <- Sys.time()
 # Create pool
 pool <- makeCluster(cores)
+# Export func
+clusterExport(pool,c("test_func","size"))
 # Apply the function n times in parallel
-res <- parLapply(cl = pool, x= 1:n, fun = test_func)
+res <- parLapply(cl = pool, X= 1:n, fun = function(x) test_func(size))
 # Terminate cluster
 stopCluster(pool)
 print(paste('Parallel time was',
-            Sys.time()-start))
-
-# Sequential
-############
-start <- Sys.time()
-res <- lapply(x = 1:n, fun = test_func)
-print(paste('Sequential time was',
             Sys.time()-start))
