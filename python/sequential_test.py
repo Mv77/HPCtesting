21c39798b47dff91b41aa2599476760f8ce66abf
MateoVG <mateovegi@gmail.com>
2020-12-14 10:00:48 -0500

    Make the test deterministic

--- python/sequential_test.py
+++ python/sequential_test.py
@@ -5,26 +5,20 @@ Created on Tue Jun 30 22:40:35 2020
 @author: Mateo
 """
 
-import numpy as np
 import time
 
-# A function to do some task
-def test_fun(size):
-    
-    # Create some size*size matrix spd matrix
-    mat = np.random.normal(size = (size,size))
-    # Get the inverse
-    inv = np.linalg.inv(np.dot(mat, mat.T))
-    # And store the sum of its elements
-    res = np.sum(inv)
-        
-    return res
+# A function to do some deterministic task:
+# Find the n-th Fibbonacci number
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 # %%
-
-n = 5000
-size = 200
+n = 33
 
 start = time.time()
-res = list(map(test_fun, [size for i in range(n)]))
+res = list(map(fib, range(n)))
 print('Sequential time was {:.{prec}} seconds'.format(time.time()-start, prec = 3))
+print(res)
